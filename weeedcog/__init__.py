d9c3b01d7d46e60ef460424eab3e961d976b4230
Ashley Davis <luna@airsi.de>
2019-02-01 13:12:06 -0500

    update info.json for repo and weeed cog, fix weeed __init__

--- weeedcog/__init__.py
+++ weeedcog/__init__.py
@@ -5,6 +5,4 @@ from shutil import rmtree
 
 def setup(bot):
     cog = WeeedBot(bot)
-    rmtree(data_manager.cog_data_path(cog))
-    data_manager.load_bundled_data(cog, __file__)
     bot.add_cog(cog)
