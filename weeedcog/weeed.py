70abadf356d72dbebe70cd66da4485e87034ff9e
Ashley Davis <luna@Ashleys-MacBook-Pro.local>
2019-08-29 15:46:46 -0400

    update message reverse method

--- weeedcog/weeed.py
+++ weeedcog/weeed.py
@@ -266,9 +266,9 @@ class WeeedBot(commands.Cog):
         else:
             anchor_msg = ctx.message
         # Get the specified number of messages using ctx.history()
-        messages = await ctx.history(before=anchor_msg,
+        messages = reversed(await ctx.history(before=anchor_msg,
                                      limit=count,
-                                     oldest_first=False).flatten().reverse()
+                                     oldest_first=False).flatten())
         # Again, if given a message ID, we need to get the history but also
         # add the message with the ID that was passed and, since we're using
         # reverse=True we append (otherwise we'd prepend)
